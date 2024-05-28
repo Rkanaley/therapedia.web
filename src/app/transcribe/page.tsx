84f9728ef735f913625291ad8aee9f52b71f1e32
Kriss Gardner <krissrobert12@gmail.com>
2024-05-28 22:39:28 +0300

    Create example

--- src/app/transcribe/page.tsx
+++ src/app/transcribe/page.tsx
@@ -5,6 +5,7 @@ import io, { Socket } from 'socket.io-client'
 import { Button } from '@/components/Button'
 
 const getWebSocketUrl = () => {
+  console.log(process.env.NEXT_PUBLIC_WS_URL)
   return process.env.NEXT_PUBLIC_WS_URL || 'ws://localhost:8000'
 }
 
