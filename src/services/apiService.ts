5a258524dbe9d8410cf13c112b658b8fd7d8fbfa
Kriss Gardner <krissrobert12@gmail.com>
2024-05-31 23:56:54 +0300

    Create api service

---
+++ src/services/apiService.ts
@@ -0,0 +1,64 @@
+import axios from 'axios'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+export const login = async (email: string, password: string) => {
+  const response = await axios.post(`${API_URL}/auth/login`, {
+    email,
+    password,
+  })
+
+  return response.data
+}
+
+export const validateToken = async (token: string) => {
+  const response = await axios.post(
+    `${API_URL}/auth/validate`,
+    { token },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  )
+
+  return response.status === 200
+}
+
+export const createTranscription = async (token: string): Promise<number> => {
+  const response = await axios.post(
+    `${API_URL}/transcriptions`,
+    { text: '' },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  )
+
+  if (response.status !== 201) {
+    throw new Error('Failed to create transcription')
+  }
+
+  const { id } = response.data
+  return id
+}
+
+export const updateTranscription = async (
+  token: string,
+  transcriptionId: number,
+  text: string,
+) => {
+  await axios.put(
+    `${API_URL}/transcriptions/${transcriptionId}`,
+    { text },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  )
+}
