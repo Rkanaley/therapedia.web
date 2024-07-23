b9b9de3aecf4e909aba7cb77faf611203833d864
Daniel <daniel.eichenberger84@gmail.com>
2024-07-22 23:31:45 -0600

    Logo debug.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export function Logo(props: React.ComponentPropsWithoutRef<'img'>) {
   return (
     <img 
-      src="/therapedia-logo.png" 
+      src="/images/image.png" 
       alt="Therapedia Logo" 
       className="h-8 w-auto" 
       {...props} 
