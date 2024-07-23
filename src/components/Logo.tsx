59d235c41d07d7537a082e8e5e45c75ee0deaa6d
Ryan Kanaley <150100613+Rkanaley@users.noreply.github.com>
2024-07-22 22:15:22 -0700

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,29 +1,15 @@
-function LogomarkPaths() {
+// src/components/Logo.js
+
+import React from 'react';
+
+export function Logo(props) {
   return (
-    <g fill="none" stroke="#38BDF8" strokeLinejoin="round" strokeWidth={3}>
-      <path d="M10.308 5L18 17.5 10.308 30 2.615 17.5 10.308 5z" />
-      <path d="M18 17.5L10.308 5h15.144l7.933 12.5M18 17.5h15.385L25.452 30H10.308L18 17.5z" />
-    </g>
-  )
+    <img 
+      src="/therapedia-logo.png" 
+      alt="Therapedia Logo" 
+      className="h-8 w-auto" 
+      {...props} 
+    />
+  );
 }
 
-export function Logomark(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg aria-hidden="true" viewBox="0 0 36 36" fill="none" {...props}>
-      <LogomarkPaths />
-    </svg>
-  )
-}
-
-export function Logo(props: React.ComponentPropsWithoutRef<'img'>) {
-  return (
-    /*<svg aria-hidden="true" viewBox="0 0 227 36" fill="none" {...props}>
-      <LogomarkPaths />
-      <text x="45" y="24" fontSize={20} fontWeight="bold" fill="#334155">
-      </text>
-      {<path d="/images/image.png" />}
-    </svg>*/
-    <img src="/images/image.png" alt="Logo" {...props} />
-    
-  )
-}
