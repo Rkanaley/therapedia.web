71641d5dc14f9217d46622f47e56409f0b4f0d7b
Daniel <daniel.eichenberger84@gmail.com>
2024-07-22 23:37:58 -0600

    Logo fix.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,12 +4,6 @@ import React from 'react';
 
 export function Logo(props: React.ComponentPropsWithoutRef<'img'>) {
   return (
-    <img 
-      src="/images/image.png" 
-      alt="Therapedia Logo" 
-      className="h-8 w-auto" 
-      {...props} 
-    />
+    <img src="image.png" alt="Therapeida Logo" {...props} />
   );
 }
-
